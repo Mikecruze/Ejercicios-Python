7ac3a4cbbd161d3c0e5cbfbcf046e9b5c2a04d28
Mikecruze <162t0307@itsm.edu.mx>
2017-05-12 18:50:36 -0500

    Create vocal.py

---
+++ vocal.py
@@ -0,0 +1,13 @@
+def vocal():
+    return()
+letra = input("Ingrese una letra>")
+    
+if(letra=='a' or letra=='e' or letra=='i' or letra=='o' or letra=='u'):
+    print(True)
+elif(letra=='A' or letra=='E' or letra=='I' or letra=='O' or letra=='U'):
+    print(True)
+
+else:
+    
+    print(False)
+  
