d35336491362851950ee52bf97709d9c01504133
Mikecruze <162t0307@itsm.edu.mx>
2017-05-12 18:51:23 -0500

    Create sum-mult.py

---
+++ sum-mult.py
@@ -0,0 +1,9 @@
+def suma(x):
+    return x
+def mult(y):
+    return y
+l=[5,3,2,9]
+x=(l[0]+l[1]+l[2]+l[3])
+y=(l[0]*l[1]*l[2]*l[3])
+print ("La suma de los elementos de la lista es= ",x)
+print ("La multiplicacion de los elementos de la lista es= ",y)
