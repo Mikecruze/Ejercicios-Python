09f54e9739c108adef30157aff854c2926044469
Mikecruze <162t0307@itsm.edu.mx>
2017-05-12 19:02:38 -0500

    Create gen-car.py

---
+++ gen-car.py
@@ -0,0 +1,12 @@
+def generar_n_caracteres(n,x):
+    return (n,x)
+
+n= int(input("Ingrese un número entero>"))
+x= input("Ingrese una letra>")
+i=0
+
+while i<n:
+    print(x)
+    i+=1
+
+print("Fin del Proceso")
